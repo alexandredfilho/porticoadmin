44d253973186764c60c5cb396ab04b5f2fb4eebe
Alexandre Domingos Filho <alexandre.filho@adfsolucoesemti.com.br>
2021-05-26 00:18:12 -0300

    Generate the migration to the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_25_175306) do
+ActiveRecord::Schema.define(version: 2021_05_26_025647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "customers", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "drivers", force: :cascade do |t|
     t.string "first_name"
@@ -30,6 +36,7 @@ ActiveRecord::Schema.define(version: 2021_05_25_175306) do
     t.string "license_plate"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "type"
   end
 
 end
